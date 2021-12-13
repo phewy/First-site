da206d15c24eefc795316865fba010f31c195284
phewy <78772119+phewy@users.noreply.github.com>
2021-12-13 12:02:27 +0200

    Experimenting

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const rrLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+const tunakLink = "https://www.youtube.com/watch?v=vTIIMJ9tUc8";
 
 function openLink(){
     window.open(rrLink)
+}
+
+function openLink2(){
+    window.open(tunakLink)
 }
